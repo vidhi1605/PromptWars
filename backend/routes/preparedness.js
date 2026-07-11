2fdf2549aa16d8f437c961dde0b877fc6bebaf56
vidhi1605 <vidhi.aggarwal0516@gmail.com>
2026-07-11 13:34:34 +0530

    Remove backend

--- backend/routes/preparedness.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { validatePreparednessBody } from '../middleware/validation.js';
-import { createPreparedness } from '../controllers/preparednessController.js';
-
-const router = Router();
-
-router.post('/preparedness-plan', validatePreparednessBody, createPreparedness);
-
-export default router;
